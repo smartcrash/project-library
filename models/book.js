af794fd0d894973efbffcfd977a9165010bb6602
Diego Da'Silva <smartcrashomg@gmail.com>
2021-11-30 13:23:42 -0400

    Add Book model

---
+++ models/book.js
@@ -0,0 +1,43 @@
+'use strict'
+const { Model, DataTypes } = require('sequelize')
+
+module.exports = sequelize => {
+  class Book extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Book.init(
+    {
+      title: DataTypes.STRING,
+      commentcount: {
+        type: DataTypes.TINYINT,
+        defaultValue: 0,
+      },
+      comments: {
+        type: DataTypes.TEXT,
+        defaultValue: '[]',
+        get() {
+          try {
+            return JSON.parse(this.getDataValue('comments')) || []
+          } catch (error) {
+            return []
+          }
+        },
+        set(value) {
+          this.setDataValue('comments', JSON.stringify(value))
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Book',
+    }
+  )
+  return Book
+}
