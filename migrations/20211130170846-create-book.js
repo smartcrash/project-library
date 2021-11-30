14f7ca8cfe25413a518f4b15988340ca2ab82c93
Diego Da'Silva <smartcrashomg@gmail.com>
2021-11-30 14:06:29 -0400

    Rename PK id => _id

--- migrations/20211130170846-create-book.js
+++ migrations/20211130170846-create-book.js
@@ -2,7 +2,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Books', {
-      id: {
+      _id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
--- models/book.js
+++ models/book.js
@@ -14,6 +14,7 @@ module.exports = sequelize => {
   }
   Book.init(
     {
+      _id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
       title: DataTypes.STRING,
       commentcount: {
         type: DataTypes.TINYINT,
@@ -37,6 +38,7 @@ module.exports = sequelize => {
     {
       sequelize,
       modelName: 'Book',
+      timestamps: true,
     }
   )
   return Book
